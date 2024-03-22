568a86520fd5e58f73cda49e9c55d0363c1e6eab
Sansan <msandartun@gmail.com>
2024-03-22 16:48:53 +0800

    enables user to select and reset image from upload image button, base64 img conversion and ensures image fits in the div

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,24 @@
 import "./Home.css";
 import placeholder from "../assets/placeholder.svg";
+import { useState } from "react";
 
 export const Home = () => {
+  const [selectedImage, setSelectedImage] = useState<any>(null);
+  const handleImageChange = (e: any) => {
+    const reader = new FileReader();
+
+    reader.readAsDataURL(e.target.files[0]);
+
+    reader.onload = () => {
+      console.log("called: ", reader);
+      setSelectedImage(reader.result);
+    };
+  };
+
+  const handleResetImage = () => {
+    setSelectedImage(null);
+  };
+
   return (
     <div className="home-container">
       <h1 className="title">Endo Diet</h1>
@@ -12,15 +29,25 @@ export const Home = () => {
 
       <div className="image-wrapper">
         <div className="image-preview">
-          <img className="image" src={placeholder} alt="Image not found" />
+          <img
+            className="image"
+            src={selectedImage ?? placeholder}
+            alt="Selected Image"
+          />
         </div>
 
         <div className="button-container">
           <label className="custom-file-upload">
-            <input type="file" className="upload-img" />
+            <input
+              type="file"
+              className="upload-img"
+              onChange={handleImageChange}
+            />
             Upload Image
           </label>
-          <button className="reset-btn">Reset</button>
+          <button className="reset-btn" onClick={handleResetImage}>
+            Reset
+          </button>
         </div>
       </div>
       <div className="process-btn-wrapper">
