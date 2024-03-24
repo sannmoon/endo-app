4a897a49de1d11126c646fa834d1dd68033812d9
Sandar <92175019+sansanlicious@users.noreply.github.com>
2024-03-24 15:04:56 +0800

    handling frontend api rq (#6)
    * handling frontend api rq
    
    * removed div tag from App.tsx

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,13 +1,22 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
+app.use(express.json({ limit: "10mb" }));
+
 app.get("/health", (req: Request, res: Response) => {
   res.send("ok");
+});
+
+app.post("/analyze-image", (req: Request, res: Response) => {
+  console.log(req.body.image);
+  res.send("done");
 });
 
 app.listen(port, () => {
