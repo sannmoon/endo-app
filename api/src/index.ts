db15ff9bc5e8eb91db36c2d1a36d2ec9abaf0872
Sansan <msandartun@gmail.com>
2024-03-24 18:34:44 +0800

    added integration with openai

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,7 +1,11 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import OpenAI from "openai";
 
+const openai = new OpenAI({
+  apiKey: process.env.OPEN_AI_API_KEY,
+});
 dotenv.config();
 
 const app: Express = express();
@@ -14,9 +18,37 @@ app.get("/health", (req: Request, res: Response) => {
   res.send("ok");
 });
 
-app.post("/analyze-image", (req: Request, res: Response) => {
-  console.log(req.body.image);
-  res.send("done");
+app.post("/analyze-image", async (req: Request, res: Response) => {
+  // console.log(req.body.image);
+
+  const response = await openai.chat.completions.create({
+    model: "gpt-4-vision-preview",
+    messages: [
+      {
+        role: "system",
+        content:
+          "You are a gynecologist that specialize in endometriosis. Analyze if this image (food) is healthy for endometriosis sufferers",
+      },
+      {
+        role: "user",
+        content: [
+          {
+            type: "text",
+            text: "Does this image containing food good for endometriosis?",
+          },
+          {
+            type: "image_url",
+            image_url: {
+              url: req.body.image,
+            },
+          },
+        ],
+      },
+    ],
+  });
+  console.log(response.choices[0].message.content);
+
+  res.send({ result: response.choices[0].message.content });
 });
 
 app.listen(port, () => {
