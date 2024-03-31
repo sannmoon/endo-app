98908639210e9790b56de8d087b01c1c90d40bf6
Sansan <msandartun@gmail.com>
2024-03-31 20:15:41 +0800

    Send login data to api

--- frontend/src/pages/Login/LoginForm.tsx
+++ frontend/src/pages/Login/LoginForm.tsx
@@ -3,23 +3,36 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import { LabeledInput } from "../../components/LabeledInput/LabeledInput";
 import { Button } from "../../components/Button/Button";
+import { useState } from "react";
+import Axios from "axios";
 
 export const LoginForm = () => {
+  const [loading, setLoading] = useState(false);
+
   const schema = yup.object().shape({
     email: yup.string().email().required("Please enter a valid email address"),
     password: yup.string().required("Please enter your password"),
   });
 
   const {
+    register,
     handleSubmit,
     formState: { errors },
   } = useForm({
     resolver: yupResolver(schema),
   });
 
-  const onSubmit = (data: any) => {
-    console.log(data);
+  const onSubmit = async (data: any) => {
+    setLoading(true);
+    try {
+      await Axios.post(`http://localhost:3000/login`, data);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
   };
+
   return (
     <form onSubmit={handleSubmit(onSubmit, () => {})}>
       <div className="form-group">
@@ -28,13 +41,15 @@ export const LoginForm = () => {
           type="email"
           placeholder="m@example.com "
           errors={errors.email?.message}
+          {...register("email")}
         />
         <LabeledInput
           label="Password"
           type="password"
           errors={errors.password?.message}
+          {...register("password")}
         />
-        <Button label="Login" />
+        <Button label={loading ? "Loading ✨" : "Login"} />
       </div>
     </form>
   );
