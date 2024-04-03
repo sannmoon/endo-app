416d38163c37453e50e8b9b8871b72f24ccd7656
Sansan <msandartun@gmail.com>
2024-04-03 11:58:26 +0800

    Adding CRUD and REST

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,6 +2,8 @@ import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import OpenAI from "openai";
+import mysql from "mysql";
+import bcrypt from "bcrypt";
 
 dotenv.config(); // ⚠️ should always be written down first before putting the env variable key//
 
@@ -15,14 +17,77 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 
+const db = mysql.createConnection({
+  host: "127.0.0.1",
+  port: 3306,
+  user: "root",
+  password: "password",
+  database: "endo-diet",
+});
+
+db.connect((err) => {
+  if (err) {
+    console.log(err);
+    console.log("Unable to connect to DB");
+  } else {
+    console.log("Database connected successfully");
+  }
+});
+
+function queryPromise(
+  sql: string | mysql.QueryOptions,
+  values: any[] = []
+): any {
+  return new Promise((resolve, reject) => {
+    db.query(sql, values, (error, results) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(results);
+      }
+    });
+  });
+}
+
 app.get("/health", (req: Request, res: Response) => {
   res.send("ok");
 });
 
-app.post("/signup", (req: Request, res: Response) => {
-  console.log(req.body);
-  console.log(req.body.email, req.body.password, req.body.repeatPassword);
-  res.send("ok");
+app.post("/signup", async (req: Request, res: Response) => {
+  try {
+    const { email, password, repeatPassword } = req.body;
+
+    const emailCheck = await queryPromise(
+      "SELECT * FROM users WHERE email = ?",
+      [email]
+    );
+
+    if (emailCheck.length > 0) {
+      // or !== 0
+      res.status(400).send({ errorMessage: "User has already exist" });
+      return;
+    }
+
+    if (password !== repeatPassword) {
+      res.status(400).json({ errorMessage: "Passwords don't match" });
+      return;
+    }
+
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    const users = [email, passwordHash];
+    // Executing the query
+    const result = await queryPromise(
+      "INSERT INTO users (email, password) VALUES (?,?)",
+      users
+    );
+    res.json({ id: result.insertId, email });
+
+    res.status(201).send("User created");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error creating user");
+  }
 });
 
 app.post("/login", (req: Request, res: Response) => {
